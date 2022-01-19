d0e70dd2cca6be80b9d7c1c10e31a2f487cc94ed
Iagao-PC <iagofmoraes@hotmail.com>
2022-01-19 17:47:10 -0300

    Inicio do projeto com entity framework.

---
+++ WebCars/Migrations/20220119190306_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebCars.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(nullable: false),
+                    Ano = table.Column<int>(nullable: false),
+                    Cor = table.Column<string>(nullable: false),
+                    Categoria = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
